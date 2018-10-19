fcc122c040929af26f7e878fd68b3d2b62c2904a
yoqu <yoqulin@qq.com>
2018-10-19 10:26:15 +0800

    优化

--- src/main/java/com/uyoqu/demo/spring/cache/demo/common/ApiRet.java
+++ src/main/java/com/uyoqu/demo/spring/cache/demo/common/ApiRet.java
@@ -15,4 +15,20 @@ public class ApiRet implements Serializable {
         this.code = code;
         this.msg = msg;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
